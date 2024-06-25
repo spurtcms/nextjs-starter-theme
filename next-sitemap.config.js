5136445b4b06afc40e5fa702c548216d4879b0da
Balamurugan-picco <95912205+spurtcmsdeveloper-144@users.noreply.github.com>
2024-06-25 18:39:22 +0530

    sidemap-checks

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+// const dev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  siteUrl: "https://nextjs-starter-theme.vercel.app/",
+};
