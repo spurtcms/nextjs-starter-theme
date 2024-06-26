44cef856ead642d38bdcb07c1bac2268e45cf952
Balamurugan-picco <95912205+spurtcmsdeveloper-144@users.noreply.github.com>
2024-06-26 16:25:59 +0530

    issue fix

--- src/app/components/Home/index.js
+++ src/app/components/Home/index.js
@@ -1,31 +1,20 @@
-// import React, { Suspense } from 'react'
+import React, { Suspense } from 'react'
 import HomePage from './Home'
-// import { GET_POSTS_CATEGORYLIST_QUERY } from '@/app/api/query'
+import { GET_POSTS_CATEGORYLIST_QUERY } from '@/app/api/query'
 import { fetchGraphQl } from '@/app/api/graphicql'
-import Link from 'next/link'
-import { GET_POSTS_LIST_QUERY } from '@/app/api/query'
 
 const HomeServerAction =async () => {
-    // let variable_category={
-    //   "hierarchylevel": 0,
-    //   "limit":50,
-    //   "offset":0
-    // }
-  // const Listdata=await fetchGraphQl(GET_POSTS_CATEGORYLIST_QUERY,variable_category)
-         let variable_list={ "limit": 10, "offset": 0,"requireData": {
-          "authorDetails": true
-        }}
-  let postdatas=await fetchGraphQl(GET_POSTS_LIST_QUERY,variable_list)
+    let variable_category={
+      "hierarchylevel": 0,
+      "limit":50,
+      "offset":0
+    }
+  const Listdata=await fetchGraphQl(GET_POSTS_CATEGORYLIST_QUERY,variable_category)
   return (
     <div>
-       {/* <Link href={"/view-all-posts"}>view-all-posts</Link>  */}
-        {/* <Suspense fallback={null}> */}
-        <Link href={"/list-page"}>list-page</Link> 
-      <HomePage postdatas={postdatas}/>
-      {/* </Suspense> */}
-      <Link href={"/view-all-posts"}>view-all-posts</Link> 
-    
-    
+        <Suspense fallback={null}>
+      <HomePage Listdata={Listdata}/>
+      </Suspense>
     </div>
   )
 }
