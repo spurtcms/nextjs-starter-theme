c6065360107d8fd865343c5e545a1389c9d677f8
Balamurugan-picco <95912205+spurtcmsdeveloper-144@users.noreply.github.com>
2024-06-26 14:54:33 +0530

    issue fix

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,10 @@ import HomeServerAction from "./components/Home";
 export default function Home() {
 
   return (
-    <> <HomeServerAction/>
+    <> 
    <Link href={"view-all-posts"}>view-all-posts</Link> 
+
+    <HomeServerAction/>
     </>
    
   );
