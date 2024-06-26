0b9b5d598f96a648cb4894bd2ab9cbb04975c5ca
Balamurugan-picco <95912205+spurtcmsdeveloper-144@users.noreply.github.com>
2024-06-26 17:48:26 +0530

    issue fix

--- src/app/components/Home/index.js
+++ src/app/components/Home/index.js
@@ -1,4 +1,4 @@
-// import React, { Suspense } from 'react'
+import React, { Suspense } from 'react'
 import HomePage from './Home'
 import { GET_POSTS_CATEGORYLIST_QUERY, GET_POSTS_LIST_QUERY } from '@/app/api/query'
 import { fetchGraphQl } from '@/app/api/graphicql'
