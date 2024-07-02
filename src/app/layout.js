cd301fa45848d6820fb8a722999659448cd244a1
Balamurugan-picco <95912205+spurtcmsdeveloper-144@users.noreply.github.com>
2024-07-02 19:15:58 +0530

    issue fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import { DarkThemeProvider } from "./utilities/Theme/DarkThemeProvider";
-
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +15,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" >
       <body className={inter.className}>
-     
+      <NextTopLoader
+            color="#00AEEF"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={4}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+            template='<div class="bar" role="bar"><div class="peg"></div></div> 
+        <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+            zIndex={1600}
+            showAtBottom={false}
+          />
         <DarkThemeProvider>
       <main className="container min-h-screen mx-auto max-w-screen-lg">
       <Header/>
