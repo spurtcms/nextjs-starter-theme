5e7e9b1faf2dc00df1232d2b658d7179841cd849
Saravanan <piccotalent065@gmail.com>
2024-11-16 14:20:03 +0530

    api url changes for testing

--- src/app/api/interceptor.js
+++ src/app/api/interceptor.js
@@ -2,7 +2,8 @@ export const apiinstance=async(url,options)=>{
     const headers = {
         'Content-Type': 'application/json',
         "Authorization":process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_TOKEN,
-        "ApiKey":process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY
+        // "ApiKey":process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY
+        "ApiKey":process.env.next_public_spurtcms_nextjs_starter_apikey
       }
     
       const config = {
