f0f177ec1480745198323d6281a4a3ab4d2f2b96
abinesh-piccosoft-react <141411308+abinesh-piccosoft-react@users.noreply.github.com>
2025-01-04 14:56:10 +0530

    deployement issue

--- src/app/api/interceptor.js
+++ src/app/api/interceptor.js
@@ -1,30 +1,30 @@
-export const apiinstance=async(url,options)=>{
-    const headers = {
-        'Content-Type': 'application/json',
-        "Authorization":process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_TOKEN,
-        // "ApiKey":process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY
-        "ApiKey":process.env.next_public_spurtcms_nextjs_starter_apikey
-      }
-    
-      const config = {
-        method: options.method || 'GET',
-        headers,
-        ...options,
-      }
-    
-      if (config.method === 'GET') {
-        delete config.body
-      } else {
-        config.body = config.body
-      }
+export const apiinstance = async (url, options) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    // "Authorization": process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_TOKEN,
+    "Apikey":"J5EVUUpzY8dO-2HZZ44kS8Y8Wu4H7VYDWVWlpt23w_Kizc8MR0tZ2bouV8wDxx23j5TweJOM1GpwZr5-OrZzLA=="
+  }
 
-  
-        
-      const res = await fetch(`${process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_BASEURL}${url}`,config);
-      if(res.ok){
-            return await res.json();
-        }else{
-         
-        }
-     
+  const config = {
+    method: options.method || 'GET',
+    headers,
+    ...options,
+  }
+
+  if (config.method === 'GET') {
+    delete config.body
+  } else {
+    config.body = config.body
+  }
+
+
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_BASEURL}${url}`, config);
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+
+
 }
