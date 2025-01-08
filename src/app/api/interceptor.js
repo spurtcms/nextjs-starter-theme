c961d0650631463acd79a6a085f4f27c2ad8ae52
abinesh-piccosoft-react <141411308+abinesh-piccosoft-react@users.noreply.github.com>
2025-01-08 12:30:34 +0530

    default key changes

--- src/app/api/interceptor.js
+++ src/app/api/interceptor.js
@@ -1,8 +1,9 @@
 export const apiinstance = async (url, options) => {
+  const key = process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY ? process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY : process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_DEFAULT_APIKEY
   const headers = {
     'Content-Type': 'application/json',
     "Authorization": process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_THEME_TOKEN,
-    "ApiKey":process.env.NEXT_PUBLIC_SPURTCMS_NEXTJS_STARTER_APIKEY
+    "ApiKey": key
   }
 
   const config = {
@@ -25,6 +26,4 @@ export const apiinstance = async (url, options) => {
   } catch (error) {
     console.log(error)
   }
-
-
 }
